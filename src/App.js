ed5635dcd63425b5bf27fc5c3b5f158f1e39e29c
Alexandr Abrosimov <mystudyjs11@gmail.com>
2020-10-01 09:23:50 +0700

    Добавил компонент глобальных стилей, назначил основной background для body

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  body {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    margin: 0;
+    background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 5e-05) 49.88%, rgba(0, 0, 0, 0.5) 100%), url('./image/bg/bg.png');
+    background-position: center center;
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;  
+    z-index: 1;
+  }
+
+
+@media only screen and (-webkit-min-device-pixel-ratio: 1.5),
+ only screen and (min-moz-device-pixel-ratio: 1.5),
+ only screen and (min-resolution: 240dpi) {
+ body {
+ background: url('./image/bg/bg@2x.png');
+
+ }
+}
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  h1, h2, h3, h4, p {
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyle />
+      <div className="App">
+
+      </div>
     </div>
   );
 }
